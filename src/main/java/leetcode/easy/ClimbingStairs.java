ad8e1ce35e7ccd3e6d14cd5a1aa587439b4ba921
TomKetipov <tom.ketipov@gmail.com>
2023-11-27 13:50:47 +0200

    Sync master with unsolved

--- src/main/java/leetcode/easy/ClimbingStairs.java
+++ src/main/java/leetcode/easy/ClimbingStairs.java
@@ -8,15 +8,12 @@ public class ClimbingStairs {
         if (n <= 0) {
             throw new InvalidParameterException("Number of stairs must be >= 1.");
         }
-
         int a = 0, b = 1, sum = a + b;
-
         while (n-- >= 2) {
             a = b;
             b = sum;
             sum = a + b;
         }
-
         return sum;
     }
 }
