a90f320d43b117909be4f9215a325258f0843d4b
TomKetipov <tom.ketipov@gmail.com>
2023-12-15 16:58:09 +0200

    Add validation service

---
+++ src/main/java/services/ValidationMessages.java
@@ -0,0 +1,11 @@
+package services;
+
+public class ValidationMessages {
+    public static final String NOT_NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "The input number must be non-negative value.";
+    public static final String NOT_POSITIVE_NUMBER_EXCEPTION_MESSAGE = "The input number must be a positive value.";
+    public static final String NOT_NULL_STRING_EXCEPTION_MESSAGE = "The input String cannot be null.";
+    public final static String NOT_NULL_ARRAY_EXCEPTION_MESSAGE = "The input array must not be null.";
+    public final static String NOT_NULL_LINKED_LIST_EXCEPTION_MESSAGE = "The input linked list must not be null.";
+    public final static String NOT_EMPTY_ARRAY_EXCEPTION_MESSAGE = "The input array must not be empty.";
+    public final static String NOT_IN_BOUNDS_OF_ARRAY_EXCEPTION_MESSAGE = "The input index is out of the array bounds.";
+}
