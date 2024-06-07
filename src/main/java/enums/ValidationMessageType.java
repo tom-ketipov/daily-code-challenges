3f947fef2d2cd80e4ee4056a2456e067c746e801
p1ne <tom.ketipov@gmail.com>
2024-06-07 09:18:59 +0300

    Update AddBinarySolver validation and unit test coverage

--- src/main/java/enums/ValidationMessageType.java
+++ src/main/java/enums/ValidationMessageType.java
@@ -2,7 +2,7 @@ package enums;
 
 public enum ValidationMessageType {
     NULL_VALUE_ERROR("The input must not be null"),
-    EMPTY_ARRAY_ERROR("The input array must not be empty"),
+    EMPTY_VALUE_ERROR("The input value must not be empty"),
     OUT_OF_RANGE_ERROR("The input value must be between %d and %d"),
     BELOW_MINIMUM_ERROR("The value must be bigger than %d (inclusively)"),
     ABOVE_MAXIMUM_ERROR("The value must be smaller than %d (inclusively)");
