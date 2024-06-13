26cd18ebd6a4b76b98961906e2b717b4cf15d33c
p1ne <tom.ketipov@gmail.com>
2024-06-13 14:37:53 +0300

    (LeetCode-easy) Add solution to #3162 with unit test coverage

---
+++ src/main/java/leetcode/easy/find_the_number_of_good_pairs_3162/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.easy.find_the_number_of_good_pairs_3162;
+
+// https://leetcode.com/problems/find-the-number-of-good-pairs-i/description/
+
+import enums.ValidationMessageType;
+import org.apache.commons.lang3.Validate;
+
+public class Solution {
+
+    public int getMinArrLength() {
+        return 1;
+    }
+
+    public int getMaxArrLength() {
+        return 50;
+    }
+
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        Validate.notNull(nums1, ValidationMessageType.NULL_VALUE_ERROR.getExceptionMessage());
+        Validate.notNull(nums2, ValidationMessageType.NULL_VALUE_ERROR.getExceptionMessage());
+
+        Validate.inclusiveBetween(getMinArrLength(), getMaxArrLength(), nums1.length,
+                ValidationMessageType.OUT_OF_RANGE_ARRAY_LENGTH_ERROR.getExceptionMessage(getMinArrLength(), getMaxArrLength()));
+        Validate.inclusiveBetween(getMinArrLength(), getMaxArrLength(), nums2.length,
+                ValidationMessageType.OUT_OF_RANGE_ARRAY_LENGTH_ERROR.getExceptionMessage(getMinArrLength(), getMaxArrLength()));
+
+        int count = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] % (nums2[j] * k) == 0) count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/leetcode/easy/find_the_number_of_good_pairs_3162/SolutionTest.java
@@ -0,0 +1,98 @@
+package leetcode.easy.find_the_number_of_good_pairs_3162;
+
+import enums.ValidationMessageType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void can_find_good_pairs_in_two_non_empty_arrays() {
+        int[] nums1 = {1, 2, 4, 12};
+        int[] nums2 = {2, 4};
+        int k = 3;
+
+        Assertions.assertEquals(2, solution.numberOfPairs(nums1, nums2, k));
+    }
+
+    @Test
+    public void numberOfPairs_throws_null_pointer_exception_for_null_input_for_the_first_array() {
+        int[] nums = {1, 3, 4};
+        int k = 1;
+
+        try {
+            solution.numberOfPairs(null, nums, k);
+        } catch (NullPointerException e) {
+            Assertions.assertEquals(ValidationMessageType.NULL_VALUE_ERROR.getExceptionMessage(), e.getMessage());
+        }
+    }
+
+    @Test
+    public void numberOfPairs_throws_null_pointer_exception_for_null_input_for_the_second_array() {
+        int[] nums = {1, 3, 4};
+        int k = 1;
+
+        try {
+            solution.numberOfPairs(nums, null, k);
+        } catch (NullPointerException e) {
+            Assertions.assertEquals(ValidationMessageType.NULL_VALUE_ERROR.getExceptionMessage(), e.getMessage());
+        }
+    }
+
+    @Test
+    public void numberOfPairs_throws_illegal_argument_exception_if_the_first_array_length_is_empty() {
+        int[] nums = {1, 3, 4};
+        int k = 1;
+
+        try {
+            solution.numberOfPairs(new int[0], nums, k);
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals(ValidationMessageType.OUT_OF_RANGE_ARRAY_LENGTH_ERROR.getExceptionMessage(solution.getMinArrLength(), solution.getMaxArrLength()), e.getMessage());
+        }
+    }
+
+    @Test
+    public void numberOfPairs_throws_illegal_argument_exception_if_the_second_array_length_is_empty() {
+        int[] nums = {1, 3, 4};
+        int k = 1;
+
+        try {
+            solution.numberOfPairs(nums, new int[0], k);
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals(ValidationMessageType.OUT_OF_RANGE_ARRAY_LENGTH_ERROR.getExceptionMessage(solution.getMinArrLength(), solution.getMaxArrLength()), e.getMessage());
+        }
+    }
+
+    @Test
+    public void numberOfPairs_throws_illegal_argument_exception_if_the_first_array_length_is_more_than_fifty_elements_long() {
+        int[] nums = {1, 3, 4};
+        int k = 1;
+
+        try {
+            solution.numberOfPairs(new int[51], nums, k);
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals(ValidationMessageType.OUT_OF_RANGE_ARRAY_LENGTH_ERROR.getExceptionMessage(solution.getMinArrLength(), solution.getMaxArrLength()), e.getMessage());
+        }
+    }
+
+    @Test
+    public void numberOfPairs_throws_illegal_argument_exception_if_the_second_array_length_is_more_than_fifty_elements_long() {
+        int[] nums = {1, 3, 4};
+        int k = 1;
+
+        try {
+            solution.numberOfPairs(new int[51], nums, k);
+        } catch (IllegalArgumentException e) {
+            Assertions.assertEquals(ValidationMessageType.OUT_OF_RANGE_ARRAY_LENGTH_ERROR.getExceptionMessage(solution.getMinArrLength(), solution.getMaxArrLength()), e.getMessage());
+        }
+    }
+}
